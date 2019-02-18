a4218a4a067004b7c04088dccaa4004e319bfef8
wxfxiaofei <327458709@qq.com>
2019-02-18 11:55:22 +0800

    girl文件方法

--- src/Girl.java
+++ src/Girl.java
@@ -1,4 +1,11 @@
 public class Girl {
     private String name;
     private int age;
+
+    public void shopping(){
+        System.out.println("购物");
+    }
+    public void rest(){
+        System.out.println("休息");
+    }
 }
